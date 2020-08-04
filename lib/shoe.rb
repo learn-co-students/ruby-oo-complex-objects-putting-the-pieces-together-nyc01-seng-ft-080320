92bd0332286c1330a54c51bb7bf36be81aa46aff
vincentcollis <vinsanity0050@gmail.com>
2020-08-04 17:19:20 -0400

    created class Shoe and class Book

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,37 @@
 # Make your shoe class here!
+
+class Shoe
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def brand
+        @brand
+    end
+    
+    def color
+        @color
+    end
+
+    def size
+        @size
+    end
+
+    def material
+        @material
+    end
+
+    def condition
+        @condition
+    end
+
+    def cobble
+        @condition = 'new'
+        puts "Your shoe is as good as new!"
+    end
+end
