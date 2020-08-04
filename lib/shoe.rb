87a20f6817c5c4688405cce0f515f8b188544d7d
kain <kaindoneson@gmail.com>
2020-08-04 08:42:46 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+#rspec spec/02_shoe_spec.rb
+
+class Shoe
+    # lol you know u need sleep when u think the lab means some boolean conditional for condition and get confused for 2 min.
+    attr_accessor :brand, :color, :size, :material, :condition;
+    
+    def initialize (brand)
+        @brand = brand
+    end
+    def cobble
+        puts 'Your shoe is as good as new!'
+        @condition = 'new';
+    end
+end
